997efa6bc36a4756af7afa56a696ac055f4955e0
shiva prasad <shiva@secondsdigital.com>
2020-02-17 18:21:32 +0530

    chat list on hover will display full name of the employee

--- application/views/chatTemplate.php
+++ application/views/chatTemplate.php
@@ -253,7 +253,7 @@
                       }
                     ?>
                        <li class="selectVendor"  id="<?=$user[0]['first_name'];?>" title="<?=$user[0]['first_name'];?>">
-                          <img onclick="ScrollDown();"style=" <?php if($i<=20){echo 'border: 2px solid #0e8016;box-shadow: 2px 2px 5px #0e8016;';};?>" src="<?=base_url('uploads/').$user[0]['profile_pic'];?>" alt="<?=$user[0]['first_name'];?>" title="<?=$user[0]['first_name']?>">
+                         <img onclick="ScrollDown();"style=" <?php if($i<=20){$count++;echo 'border: 2px solid #0e8016;box-shadow: 2px 2px 5px #0e8016;';}?>" src="<?=base_url('uploads/').$user[0]['profile_pic'];?>" alt="<?=$user[0]['first_name'];?>" title="<?=$user[0]['first_name'].' '.$user[0]['last_name']?>">
                           <a class="users-list-name" href="#"><?=$user[0]['first_name'];?></a>
                           <!--<span class="users-list-date">Yesterday</span>-->
                         </li>
